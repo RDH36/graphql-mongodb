7ecedab86a65170b099f68c2f80d4666d0993a40
raymond dzery hago <raymond.rakotonirainy@bubble-go.mg>
2024-05-31 10:37:31 +0300

    feat: add dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
